e0ac791d69af9108f71e2e9fee7444e6df6458da
Emma Cotter <ecotter@uw.edu>
2016-02-10 13:08:37 -0800

    Added tests

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,5 +1,2 @@
 from . import pronto_utils
 from . import basic_utils
-
-# call this as myproject.PI
-PI = 3.14159
---
+++ myproject/basic_utils.py
@@ -0,0 +1,5 @@
+from .constants import PI
+
+
+def multiply_by_10(x):
+    return x*10
